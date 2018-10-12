808f5a85a6ffd5be22b2bc1d37f87e3fda240dce
huxiaofeicn <179186438@qq.com>
2018-10-12 11:39:52 +0800

    修改地图配色

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -17,12 +17,19 @@ function convolve(context) {
         r = inputData[i];
         g = inputData[i+1];
         b = inputData[i+2];
-
-        var v = 0.299 * r+ 0.587 * g + 0.114 *b;
         
-        pix[i] = 180 - r;
-        pix[i+1] = 220 - g;
-        pix[i+2] = 280 - b;
+        
+        r =  Math.min(Math.abs(r - 234),Math.abs( r - 163)) *3;
+        var b2 =  Math.abs(g - 230)*4;
+        var g2 =  Math.abs(b - 230)*2;
+        
+        pix[i] = (r);
+        pix[i+1] = (g2);
+        pix[i+2] = (b2);
+        
+        //pix[i] = parseInt(r);
+        //pix[i+1] = parseInt(g);
+        //pix[i+2] = parseInt(b);
         pix[i+3] = 255;
         
     }
